b43497b98cc660d3677affd196eb45aedeb4d6d6
khimaros <me@khimaros.com>
2021-12-07 17:32:44 -0800

    update to 12.0

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -2,20 +2,17 @@
 
 cd "${AOSP_BUILD_DIR}"
 
-# apply microg sigspoof patch
-echo "applying microg sigspoof patch"
-patch -p1 --no-backup-if-mismatch < "platform/prebuilts/microg/00002-microg-sigspoof.patch"
+echo "applying microg patches"
+cd "${AOSP_BUILD_DIR}/packages/modules/Permission"
+patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00001-fake-package-sig.patch"
+cd "${AOSP_BUILD_DIR}/frameworks/base"
+patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00002-microg-sigspoof.patch"
 
 # apply community patches
 echo "applying community patches"
 community_patches_dir="${ROOT_DIR}/community_patches"
 rm -rf "${community_patches_dir}"
 git clone https://github.com/rattlesnakeos/community_patches "${community_patches_dir}"
-patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00001-global-internet-permission-toggle.patch"
+#patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00001-global-internet-permission-toggle.patch"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00005-2-button-navigation.patch"
 #patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00007-set-seedvault-as-dftl-bkp-provider.patch"
-
-# apply custom hosts file
-#custom_hosts_file="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-#echo "applying custom hosts file ${custom_hosts_file}"
-#retry wget -q -O "${AOSP_BUILD_DIR}/system/core/rootdir/etc/hosts" "${custom_hosts_file}"
